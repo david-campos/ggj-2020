0e3d8aabe9f735df50294300d3feb38565e13497
david-campos <david.campos.r96@gmail.com>
2020-01-31 21:40:41 +0100

    Goofing around with some things (reloading in a trigger changing the color of the glasses and whatever)

---
+++ Assets/Scripts/PlayerLoading.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerLoading : MonoBehaviour
+{
+    public int maxLoad;
+    public Color loadedColor;
+    public Color notLoadedColor;
+    
+    private int loadAmount = 0;
+    private SkinnedMeshRenderer glassesRenderer;
+    private static readonly int Albedo = Shader.PropertyToID("_Color");
+
+    public int LoadAmount {
+        get { return loadAmount; }
+        set { loadAmount = value; }
+    }
+
+    public void Reload() {
+        loadAmount = maxLoad;
+    }
+    
+    void Start() {
+        glassesRenderer = transform.GetChild(1).GetComponent<SkinnedMeshRenderer>();
+    }
+
+    void Update() {
+        if (loadAmount > 0) {
+            if (!glassesRenderer.material.GetColor(Albedo).Equals(loadedColor)) {
+                glassesRenderer.material.SetColor(Albedo, loadedColor);
+            }
+        } else {
+            if (!glassesRenderer.material.GetColor(Albedo).Equals(notLoadedColor)) {
+                glassesRenderer.material.SetColor(Albedo, notLoadedColor);
+            }
+        }
+    }
+}
--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -12,6 +12,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private Vector3 m_CamForward;             // The current forward direction of the camera
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+        public string player;
 
         
         private void Start()
@@ -46,8 +47,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private void FixedUpdate()
         {
             // read inputs
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            float h = CrossPlatformInputManager.GetAxis("Horizontal" + player);
+            float v = CrossPlatformInputManager.GetAxis("Vertical"+ player);
             bool crouch = Input.GetKey(KeyCode.C);
 
             // calculate move direction to pass to character
