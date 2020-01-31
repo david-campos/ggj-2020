729949e7d959c529d62a8dc520d215231c1600a2
david-campos <david.campos.r96@gmail.com>
2020-01-31 23:06:24 +0100

    Now the player can shoot!

--- Assets/Scripts/PlayerLoading.cs
+++ Assets/Scripts/PlayerLoading.cs
@@ -2,39 +2,73 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityStandardAssets.Effects;
 
 public class PlayerLoading : MonoBehaviour
 {
     public int maxLoad;
     public Color loadedColor;
     public Color notLoadedColor;
-    
-    private int loadAmount = 0;
-    private SkinnedMeshRenderer glassesRenderer;
+    public Transform aimingOrigin;
+    public GameObject aimingGhost;
+    public float aimingAngle;
+    public float aimDistance;
+    public float shootingCooldown;
+    public GameObject shootPrefab;
+
+    private int m_LoadAmount = 0;
+    private SkinnedMeshRenderer m_GlassesRenderer;
     private static readonly int Albedo = Shader.PropertyToID("_Color");
+    private float m_NextShoot = 0;
 
     public int LoadAmount {
-        get { return loadAmount; }
-        set { loadAmount = value; }
+        get { return m_LoadAmount; }
     }
 
     public void Reload() {
-        loadAmount = maxLoad;
+        m_LoadAmount = maxLoad;
     }
-    
+
     void Start() {
-        glassesRenderer = transform.GetChild(1).GetComponent<SkinnedMeshRenderer>();
+        m_GlassesRenderer = transform.GetChild(1).GetComponent<SkinnedMeshRenderer>();
+        aimingOrigin.rotation = Quaternion.Euler(aimingAngle, 0, 0);
     }
 
     void Update() {
-        if (loadAmount > 0) {
-            if (!glassesRenderer.material.GetColor(Albedo).Equals(loadedColor)) {
-                glassesRenderer.material.SetColor(Albedo, loadedColor);
+        if (m_LoadAmount > 0) {
+            if (!m_GlassesRenderer.material.GetColor(Albedo).Equals(loadedColor)) {
+                m_GlassesRenderer.material.SetColor(Albedo, loadedColor);
+            }
+
+#if UNITY_EDITOR
+			// helper to visualise the ground check ray in the scene view
+            var position = aimingOrigin.position;
+            Debug.DrawLine(position, position + aimingOrigin.TransformDirection(Vector3.forward)* aimDistance);
+#endif
+            RaycastHit hitInfo;
+            aimingGhost.SetActive(Physics.Raycast(aimingOrigin.position,
+                aimingOrigin.TransformDirection(Vector3.forward), out hitInfo, aimDistance));
+            if (aimingGhost.activeSelf) {
+                aimingGhost.transform.position = hitInfo.point;
+                float rotation = Time.deltaTime * 45;
+                aimingGhost.transform.Rotate(rotation * 0.5f, rotation * 1.5f, rotation);
+                if (Input.GetButton("FireB")) {
+                    Fire();
+                }
             }
         } else {
-            if (!glassesRenderer.material.GetColor(Albedo).Equals(notLoadedColor)) {
-                glassesRenderer.material.SetColor(Albedo, notLoadedColor);
+            aimingGhost.SetActive(false);
+            if (!m_GlassesRenderer.material.GetColor(Albedo).Equals(notLoadedColor)) {
+                m_GlassesRenderer.material.SetColor(Albedo, notLoadedColor);
             }
+        }
+    }
+
+    private void Fire() {
+        if (Time.time > m_NextShoot) {
+            Instantiate(shootPrefab, aimingGhost.transform.position, aimingGhost.transform.rotation);
+            m_LoadAmount -= 1;
+            m_NextShoot = Time.time + shootingCooldown;
         }
     }
 }
