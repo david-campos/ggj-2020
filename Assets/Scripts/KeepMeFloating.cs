6c34a4448ed3e29ca01be886c2f85952797c0080
david-campos <david.campos.r96@gmail.com>
2020-02-02 14:37:49 +0100

    Tracking sinking of the boat

---
+++ Assets/Scripts/KeepMeFloating.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepMeFloating : MonoBehaviour
+{
+    public GameObject endGameScreen;
+
+    void Update() {
+        // -15 looks like a fair position to say you have sunk
+        if (transform.position.y < -15) {
+            endGameScreen.SetActive(true);
+            TimeCounter.GetInstance().StopCounting();
+            Time.timeScale = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/TimeCounter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditorInternal;
+using UnityEngine;
+
+public class TimeCounter
+{
+    private static TimeCounter _sInstance = null;
+    
+    private float m_Start = -1f;
+    private float m_End;
+
+    private TimeCounter() {}
+
+    public static TimeCounter GetInstance() {
+        return _sInstance ?? (_sInstance = new TimeCounter());
+    }
+
+    public float Seconds => m_Start > 0
+        ? (m_End > 0 ? m_End - m_Start : Time.time - m_Start)
+        : 0f;
+
+    public void StartCounting() {
+        m_Start = Time.time;
+    }
+
+    public void StopCounting() {
+        m_End = Time.time;
+    }
+}
